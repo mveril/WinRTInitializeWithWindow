3c70c308075710607227e1c337655c22ac1fdecd
mveril <mika.veril@wanadoo.fr>
2020-06-11 16:51:11 +0200

    Add WinUI support

---
+++ samples/WinUI/HellowWorld.WinUI/HellowWorld.WinUI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.UI.Xaml;
+using mveril.WinRT.InitializeWithWindow.WPF;
+using Windows.UI.Popups;
+
+// The Blank Window item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace HellowWorld.WinUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new MessageDialog("Hellow World", $"UWP {nameof(MessageDialog)}");
+            this.InitializeWinRTChild(dlg.ThisPtr);
+            await dlg.ShowAsync();
+        }
+    }
+}
---
+++ src/WinRTInitializeWithWindow.WinUi/WinUIWindowExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.UI.Xaml;
+using mveril.WinRT.InitializeWithWindow.Core;
+using mveril.WinRT.InitializeWithWindow.WinUI.CSWinRT;
+
+namespace mveril.WinRT.InitializeWithWindow.WPF
+{
+#pragma warning disable CS1591
+    public static class WinUIWindowExtension
+#pragma warning restore CS1591
+    {
+        /// <summary>
+        /// Extension method to initialize WinRT object with this <see cref="Window"/>
+        /// </summary>
+        /// <param name="window"></param>
+        /// <param name="winRTPtr">The <see cref="IntPtr"/> for the WinRT object</param>
+        public static void InitializeWinRTChild(this Window window,IntPtr winRTPtr)
+        {
+            WindowWrapper ww = WindowWrapper.FromAbi(window.ThisPtr);
+            Helper.Initialize(ww.WindowHandle, winRTPtr);
+        }
+    }
+}
